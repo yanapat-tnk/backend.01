06a812bbc3a0850a0c27f1ed294c5a2869a6f924
yanapat-tnk <67319010044@tnk.ac.th>
2024-11-05 09:49:11 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
-print('hello')
-print('Yanapat Nontarit')
-print('Aom')
-print('19')
-print('3')
-print('67319010044')
-print('play with mycat')
+from time import sleep
+i = 0
+while True:
+    i += 1
+    print('hello')
+    print('Yanapat Nontarit')
+    print('Aom')
+    print('19')
+    print('3')
+    print('67319010044')
+    print('play with mycat')
+    sleep(5)
